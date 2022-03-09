867c6e6db7cbc116be945706d5e0888aa6d645a8
monis4567 <steen.knudsen@niva-dk.dk>
2022-03-09 20:54:22 +0100

    commit_2022mar09_2054

--- Rcode01_get_taglist_from_xls_file.R
+++ Rcode01_get_taglist_from_xls_file.R
@@ -6,7 +6,7 @@
 # GBS Population genetics on thorny skates
 library(readxl)
 # define working directory
-wd00 <- "/home/hal9000/Documents/shrfldubuntu18/invertebrate_novogene_2022mar"
+wd00 <- "/home/hal9000/Documents/shrfldubuntu18/invertebrate_eDNA_freshwater_samples"
 
 # define input file
 inf01 <- "tagged_primers_BF1_BR1_for_eDNA_metabarcod_invertebr_2021oct26.xlsx"
@@ -16,8 +16,10 @@ pthinf01 <- paste0(wd00,"/",inf01)
 tibl_inx01 <- readxl::read_xlsx(pthinf01, skip=2)
 #substitute spaces underscores
 colnames(tibl_inx01) <- gsub(" ","_",colnames(tibl_inx01) )
-
-
+# define columns to keep
+ke <- c("name_F...6","tag_seq","tag_seq")
+# subset to only include specified columns
+tibl_i02 <- tibl_inx01[ke]
 
 #https://catchenlab.life.illinois.edu/stacks/manual/#prun
 outfl1 = "part01A_tag01_96_BF1BR1.txt"
@@ -25,7 +27,7 @@ outfl1 = "part01A_tag01_96_BF1BR1.txt"
 pthoutf01 <- paste0(wd00,"/",outfl1)
 
 # use tab as separator
-write.table(tibl_inx01, file=pthoutf01, sep="\t",
+write.table(tibl_i02, file=pthoutf01, sep="\t",
             row.names = F, # do not use row names
             col.names = F, # do not use columns names
             quote = F) # do not use quotes
